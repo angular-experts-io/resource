07931de74a928af92e59fbe879dbb0d53f94b2fe
Tomas Trajan <tomas.trajan@gmail.com>
2025-05-19 17:53:28 +0200

    test(resource): add read with params tests

--- projects/resource/src/lib/resource/resource.spec.ts
+++ projects/resource/src/lib/resource/resource.spec.ts
@@ -1,10 +1,10 @@
 import { jest } from '@jest/globals';
 import { TestBed } from '@angular/core/testing';
 import { HttpClient } from '@angular/common/http';
+import { Injector, runInInjectionContext, signal } from '@angular/core';
+import { of, throwError } from 'rxjs';
 
 import { restResource } from './resource';
-import { Injector, runInInjectionContext } from '@angular/core';
-import { of, throwError } from 'rxjs';
 
 describe('Rest Resource', () => {
   let injector: Injector;
@@ -123,21 +123,74 @@ describe('Rest Resource', () => {
           expect(resource.errorRead()).toBe(undefined);
         });
       });
+
+      describe('Read with params', () => {
+        const paramsId = signal<string | undefined>(undefined);
+
+        beforeEach(() => {
+          paramsId.set(undefined);
+          mockGet.mockImplementation((...args) => {
+            const url = args[0] as string;
+            if (url.includes('id=1')) {
+              return of([TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY[0]]);
+            } else if (url.includes('id=2')) {
+              return of([TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY[1]]);
+            } else {
+              return of(TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY);
+            }
+          });
+          runInInjectionContext(injector, () => {
+            resource = restResource<EntityWithIdInIdProperty, 'string'>(
+              'some/api',
+              {
+                params: () => `&id=${paramsId()}`,
+              },
+            );
+          });
+        });
+
+        it('loads all values when no id was specified', async () => {
+          await tick();
+          expect(resource.hasValues()).toBe(true);
+          expect(resource.values()?.length).toBe(2);
+          expect(resource.hasValue()).toBe(false);
+          expect(resource.value()).toBe(undefined);
+        });
+
+        it('reloads items on params change', async () => {
+          await tick();
+          expect(resource.values()?.length).toBe(2);
+
+          paramsId.set('1');
+          await tick();
+          expect(resource.hasValues()).toBe(true);
+          expect(resource.values()?.length).toBe(1);
+          expect(resource.hasValue()).toBe(true);
+          expect(resource.value()).toEqual(
+            TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY[0],
+          );
+        });
+      });
     });
   });
 });
+
+async function tick() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true);
+    });
+  });
+}
 
 interface EntityWithIdInIdProperty {
   id: string;
   name: string;
 }
 
-async function tick() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, 0);
-  });
+interface EntityWithIdInOtherIdProperty {
+  otherId: string;
+  name: string;
 }
 
 const TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY: EntityWithIdInIdProperty[] = [
@@ -150,3 +203,15 @@ const TEST_ENTITIES_WITH_ID_IN_ID_PROPERTY: EntityWithIdInIdProperty[] = [
     name: 'Test Entity 2',
   },
 ];
+
+const TEST_ENTITIES_WITH_ID_IN_OTHER_ID_PROPERTY: EntityWithIdInOtherIdProperty[] =
+  [
+    {
+      otherId: '1',
+      name: 'Test Entity 1',
+    },
+    {
+      otherId: '2',
+      name: 'Test Entity 2',
+    },
+  ];
