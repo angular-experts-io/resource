8d0dcdc83092ea58a5d3b0eeabe917c8f8220947
Tomas Trajan <tomas.trajan@gmail.com>
2025-05-27 20:06:41 +0200

    feat(resource): refactor and extract resource model and util functions

--- projects/resource/src/lib/resource/public-api.ts
+++ projects/resource/src/lib/resource/public-api.ts
@@ -1,1 +1,3 @@
 export * from './resource';
+export * from './resource.model';
+export * from './resource.util';
--- projects/resource/src/lib/resource/resource.ts
+++ projects/resource/src/lib/resource/resource.ts
@@ -1,26 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import {
   inject,
-  DestroyRef,
   signal,
   computed,
   ResourceRef,
   linkedSignal,
 } from '@angular/core';
-import { takeUntilDestroyed, rxResource } from '@angular/core/rxjs-interop';
-import {
-  Subject,
-  concatMap,
-  tap,
-  Observable,
-  mergeMap,
-  switchMap,
-  exhaustMap,
-  catchError,
-  map,
-} from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { tap, catchError, map } from 'rxjs';
 
-const LOG_PREFIX = `[@angular-experts/resource]`;
+import { behaviorToOperator, streamify } from './resource.util';
+import { RestResourceOptions, RequestType, LOG_PREFIX } from './resource.model';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function restResource<T, ID, E = any>(
@@ -215,9 +205,8 @@ export function restResource<T, ID, E = any>(
     options: RestResourceOptions<T, ID>,
   ) {
     if (
-      (options[requestType]?.strategy ??
-        options.strategy ??
-        'pessimistic') === 'pessimistic' ||
+      (options[requestType]?.strategy ?? options.strategy ?? 'pessimistic') ===
+        'pessimistic' ||
       options.params
     ) {
       resource.reload();
@@ -283,161 +272,7 @@ export function restResource<T, ID, E = any>(
   };
 }
 
-function streamify<T extends unknown[]>(
-  impl: (stream: Observable<T>) => Observable<unknown>,
-) {
-  const destroyRef = inject(DestroyRef);
-  const subject = new Subject<T>();
-  impl(subject).pipe(takeUntilDestroyed(destroyRef)).subscribe();
-  return (...args: T) => {
-    subject.next(args);
-  };
-}
-
-function behaviorToOperator(behavior: Behavior = 'concat') {
-  switch (behavior) {
-    case 'concat':
-      return concatMap;
-    case 'merge':
-      return mergeMap;
-    case 'switch':
-      return switchMap;
-    case 'exhaust':
-      return exhaustMap;
-  }
-}
-
-export type RequestType = 'create' | 'update' | 'remove';
-export type Behavior = 'concat' | 'merge' | 'switch' | 'exhaust';
-export type Strategy = 'optimistic' | 'pessimistic';
-
-export interface RestResourceOptions<T, ID> {
-  /**
-   * Whether to log verbose information to the console.
-   * This is useful for debugging and understanding the resource's behavior.
-   *
-   * @default false
-   */
-  verbose?: boolean;
-  /**
-   * A reactive function which determines the request to be made.
-   * Whenever the params change, the loader will be triggered to fetch a new value for the resource.
-   *
-   * (works the same way as Angular's `resource`)
-   *
-   * @returns A string containing URL query parameters or undefined
-   */
-  params?: () => string | undefined;
-
-  /**
-   * Function to extract the ID from an item when the ID is not stored in the `id` field.
-   * Used to identify items for update and remove operations.
-   *
-   * @param item The item from which to extract the ID
-   * @returns The ID of the item
-   */
-  idSelector?: (item: T) => ID;
-
-  /**
-   * The default strategy for the resource for every request type,
-   * it can be overridden by the request type specific strategy
-   *
-   * @default 'pessimistic'
-   */
-  strategy?: Strategy;
-  create?: {
-    /**
-     * Defines how create requests are handled when multiple requests are made.
-     * Controls the RxJS flattening operator used for the HTTP request.
-     *
-     * @default 'concat'
-     */
-    behavior?: Behavior;
-    /**
-     * Determines whether changes are applied optimistically (before server confirmation)
-     * or pessimistically (after server confirmation) for create operations.
-     *
-     * @default The value of the global strategy option
-     */
-    strategy?: Strategy;
-    /**
-     * Optionally generate a new item ID (if the target API does not handle this)
-     * and set it on the item.
-     *
-     * @property generator A function that returns a new ID (e.g., UUID or auto-increment).
-     *                     Used when the backend does not assign one automatically.
-     *
-     * @property setter (Optional) A function that sets the generated ID onto the item,
-     *                  especially useful when the item’s ID field is not named `id`.
-     *
-     * @example
-     * {
-     *   id: {
-     *     generator: () => uuid(),
-     *     setter: (id, item) => {
-     *       item.nonstandardId = id
-     *     }
-     *   }
-     * }
-     *
-     */
-    id?: {
-      /**
-       * A function that generates a new unique ID.
-       * @example
-       * () => uuid()
-       */
-      generator: () => ID;
-      /**
-       * (Optional) Custom logic for assigning the generated ID to the item.
-       *
-       * @param id The generated ID
-       * @param item The item object to modify
-       *
-       * @example
-       * setter: (id, item) => {
-       *   item.nonstandardId = id
-       * }
-       */
-      setter?: (id: ID, item: Partial<T>) => void;
-    };
-  };
-  update?: {
-    /**
-     * Defines how update requests are handled when multiple requests are made.
-     * Controls the RxJS flattening operator used for the HTTP request.
-     *
-     * @default 'concat'
-     */
-    behavior?: Behavior;
-    /**
-     * Determines whether changes are applied optimistically (before server confirmation)
-     * or pessimistically (after server confirmation) for update operations.
-     *
-     * @default The value of the global strategy option
-     */
-    strategy?: Strategy;
-  };
-  remove?: {
-    /**
-     * Defines how remove requests are handled when multiple requests are made.
-     * Controls the RxJS flattening operator used for the HTTP request.
-     *
-     * @default 'concat'
-     */
-    behavior?: Behavior;
-    /**
-     * Determines whether changes are applied optimistically (before server confirmation)
-     * or pessimistically (after server confirmation) for remove operations.
-     *
-     * @default The value of the global strategy option
-     */
-    strategy?: Strategy;
-  };
-}
-
 // TODO
-// id selector for update
 // single item mode? eg computed item if array is length of 1 else undefined ?
 // CQRS vs single item return mode ?
 // return back observable to notify completion (for orchestration)
